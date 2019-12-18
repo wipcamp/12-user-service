942eb178f982482abb591f413c7a3a6ecc4c6106
pipe34150 <pipe34150@hotmail.com>
2019-12-18 16:21:56 +0700

    [ADD] UserService

--- src/main/java/com/wipcamp/userservice/services/UserService.java
+++ src/main/java/com/wipcamp/userservice/services/UserService.java
@@ -1,7 +1,15 @@
 package com.wipcamp.userservice.services;
 
+import com.wipcamp.userservice.models.User;
 import com.wipcamp.userservice.repositories.UserRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+	@Autowired
 	UserRepository userRepository;
+
+	public User addUser(User newUser) { return userRepository.save(newUser); }
 }
