ac75e6b59d31dce4ac9695e299411b748a1442fb
Unknows <firstpan555@gmail.com>
2019-12-18 14:53:33 +0700

    [ADD]Major Entity

---
+++ src/main/java/com/wipcamp/userservice/models/Major.java
@@ -0,0 +1,38 @@
+package com.wipcamp.userservice.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Major {
+	@Id
+	@GeneratedValue
+	private long id;
+	private String name;
+	private String description;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
