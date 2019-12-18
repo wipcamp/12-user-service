afc0961db85b3111726a8d17b0386b69d3951559
pipe34150 <pipe34150@hotmail.com>
2019-12-18 16:59:07 +0700

    [UPDATE] UserController with Example GetMapping

--- src/main/java/com/wipcamp/userservice/controllers/UserController.java
+++ src/main/java/com/wipcamp/userservice/controllers/UserController.java
@@ -6,8 +6,11 @@ import com.wipcamp.userservice.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -16,6 +19,17 @@ import javax.validation.Valid;
 public class UserController {
 	@Autowired
 	UserService userService;
+
+	//Example of Mapping User
+	@GetMapping("/user/{userId}")
+	@ResponseBody
+	public User getUser(@PathVariable int userId) {
+		try{
+			return userService.findById(userId)	;
+		} catch(Exception ex){
+			return new User();
+		}
+	}
 
 	@PostMapping("/user/{userId}")
 	public ResponseEntity<User> register(@RequestBody @Valid User newUser){
