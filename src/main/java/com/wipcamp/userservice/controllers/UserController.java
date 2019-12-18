0d35fe82a40a97c45ccda047091a91f1e91f7f55
pipe34150 <pipe34150@hotmail.com>
2019-12-18 16:21:44 +0700

    [ADD] UserController

---
+++ src/main/java/com/wipcamp/userservice/controllers/UserController.java
@@ -0,0 +1,24 @@
+package com.wipcamp.userservice.controllers;
+
+import com.wipcamp.userservice.models.User;
+import com.wipcamp.userservice.services.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+public class UserController {
+	@Autowired
+	UserService userService;
+
+	@PostMapping("/user/${userId}")
+	public ResponseEntity<User> register(@RequestBody @Valid User newUser){
+		return new ResponseEntity<User>(userService.addUser(newUser) , HttpStatus.CREATED);
+	}
+}
