33e9e4cbb82796c0cb36cd77ec83e63d08d2fd48
Nope <o_o.toto@windowslive.com>
2019-12-19 17:20:33 +0700

    [UPDATE] update validation request for storeAnswer

--- src/main/java/com/wipcamp/userservice/requests/StoreAnswerRequest.java
+++ src/main/java/com/wipcamp/userservice/requests/StoreAnswerRequest.java
@@ -2,13 +2,30 @@ package com.wipcamp.userservice.requests;
 
 import com.wipcamp.userservice.models.Answer;
 
+import javax.validation.Valid;
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Size;
 
 import java.util.List;
 
 public class StoreAnswerRequest {
-	@NotNull
-	@NotBlank(message = "Request must contain array of UserAnswer")
-	private List<Answer> userAnswer;
+
+
+	@NotNull(message = "Required answers field")
+	@NotEmpty(message = "Request answers field cannot	 be empty or null")
+	@Size(min = 1,message = "Request answers must have at least 1 answer")
+	private List<Answer> answers;
+
+	public List<Answer> getAnswers() {
+		return answers;
+	}
+
+	public void setAnswers(List<Answer> answers) {
+		this.answers = answers;
+	}
 }
