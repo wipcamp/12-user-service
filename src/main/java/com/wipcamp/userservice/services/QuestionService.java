684637aef8d047f01a77777fa6ad57e215587c1b
pipe34150 <pipe34150@hotmail.com>
2019-12-20 14:27:16 +0700

    [UPDATE] Question Service with getQuestion

--- src/main/java/com/wipcamp/userservice/services/QuestionService.java
+++ src/main/java/com/wipcamp/userservice/services/QuestionService.java
@@ -1,12 +1,31 @@
 package com.wipcamp.userservice.services;
 
+import com.wipcamp.userservice.models.Question;
 import com.wipcamp.userservice.repositories.QuestionRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class QuestionService {
 	@Autowired
 	QuestionRepository questionRepository;
+
+	public QuestionService(QuestionRepository questionRepository){
+		this.questionRepository = questionRepository;
+	}
+
+	public Question getSingleQuestion(Long questionId){
+		return questionRepository.findById(questionId).get();
+	}
+
+	public List<Question> getAllQuestion(){
+		return questionRepository.findAll();
+	}
+
+	public List<Question> getQuestionListByMajorId(Long majorId){
+		return questionRepository.findAllQuestionByMajorId(majorId);
+	}
 }
