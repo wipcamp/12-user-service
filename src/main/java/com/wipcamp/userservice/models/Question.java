54cade4eae317e6f3aa86efbd9e7e018a17f60c9
pipe34150 <pipe34150@hotmail.com>
2019-12-20 14:25:50 +0700

    [UPDATE] Getter and setter in question model and json ref in one to many

--- src/main/java/com/wipcamp/userservice/models/Question.java
+++ src/main/java/com/wipcamp/userservice/models/Question.java
@@ -1,5 +1,8 @@
 package com.wipcamp.userservice.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import org.springframework.lang.NonNull;
 
 import javax.persistence.Column;
@@ -24,10 +27,45 @@ public class Question {
 	@NonNull
 	private String name;
 
+	@JsonBackReference
 	@ManyToOne
 	@JoinColumn(name="major_id",referencedColumnName = "id")
 	private Major major;
 
+
 	@OneToMany(mappedBy = "question")
 	private List<Answer> answerList;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@NonNull
+	public String getName() {
+		return name;
+	}
+
+	public void setName(@NonNull String name) {
+		this.name = name;
+	}
+
+	public Major getMajor() {
+		return major;
+	}
+
+	public void setMajor(Major major) {
+		this.major = major;
+	}
+
+	public List<Answer> getAnswerList() {
+		return answerList;
+	}
+
+	public void setAnswerList(List<Answer> answerList) {
+		this.answerList = answerList;
+	}
 }
