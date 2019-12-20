8b54d0adb40a10ab5bed47831aecda1518221130
Nope <o_o.toto@windowslive.com>
2019-12-20 21:12:19 +0700

    [UPDATE] answer entity and add validation also getter setter

--- src/main/java/com/wipcamp/userservice/models/Answer.java
+++ src/main/java/com/wipcamp/userservice/models/Answer.java
@@ -1,5 +1,8 @@
 package com.wipcamp.userservice.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import org.springframework.lang.NonNull;
 
 import javax.persistence.Column;
@@ -9,25 +12,73 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.AssertFalse;
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "answers")
 public class Answer {
+	@JsonIgnore
 	@Id
 	@GeneratedValue
 	private int id;
 
-	@NonNull
+	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name="user_id",referencedColumnName = "id")
 	private User user;
 
+	@JsonBackReference
 	@NonNull
 	@ManyToOne
 	@JoinColumn(name="question_id",referencedColumnName = "id")
 	private Question question;
 
+	@NotNull
+	@NotBlank
 	@NonNull
 	private String answerContent;
+
+	public Answer() {
+	}
+
+	public Answer(User user, @NotNull @NotBlank Question question, @NotNull @NotBlank String answerContent) {
+		this.user = user;
+		this.question = question;
+		this.answerContent = answerContent;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Question getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+
+	public String getAnswerContent() {
+		return answerContent;
+	}
+
+	public void setAnswerContent(String answerContent) {
+		this.answerContent = answerContent;
+	}
 }
