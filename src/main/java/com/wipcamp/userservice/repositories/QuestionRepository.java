cb0c87c377664d083d114ff5844ea99464dc8b6c
pipe34150 <pipe34150@hotmail.com>
2019-12-20 14:27:42 +0700

    [UPDATE] QuestionRepo with query by major Id

--- src/main/java/com/wipcamp/userservice/repositories/QuestionRepository.java
+++ src/main/java/com/wipcamp/userservice/repositories/QuestionRepository.java
@@ -3,9 +3,16 @@ package com.wipcamp.userservice.repositories;
 import com.wipcamp.userservice.models.Question;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question,Long> {
+
+	@Query("FROM Question q where q.major.id = :majorId")
+	List<Question> findAllQuestionByMajorId(@Param("majorId") Long majorId);
 
 }
