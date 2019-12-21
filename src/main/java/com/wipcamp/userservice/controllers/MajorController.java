2236cb5914a5c725b43b9a547ea0df0601ae49c9
pipe34150 <pipe34150@hotmail.com>
2019-12-21 15:28:44 +0700

    [UPDATE] Major Controller with new Response Entity

--- src/main/java/com/wipcamp/userservice/controllers/MajorController.java
+++ src/main/java/com/wipcamp/userservice/controllers/MajorController.java
@@ -1,7 +1,5 @@
 package com.wipcamp.userservice.controllers;
 
-import com.wipcamp.userservice.models.Major;
-
 import com.wipcamp.userservice.services.MajorService;
 
 import com.wipcamp.userservice.utils.ResponseForm;
@@ -9,7 +7,6 @@ import com.wipcamp.userservice.utils.ResponseForm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-
-import java.util.List;
-import java.util.NoSuchElementException;
 
 @CrossOrigin("${CROSSSITEDOMAIN}")
 @RestController
@@ -45,7 +39,7 @@ public class MajorController {
 	@GetMapping("")
 	public ResponseEntity<ResponseForm> getAllMajor(HttpServletRequest request){
 		ResponseForm result = majorService.getAllMajor(request);
-		return new ResponseEntity<>(result, result.getCode());
+		return new ResponseEntity<>(result, result.getHttpCode());
 	}
 
 	//Get single major when mapping major_id , return status found when major_id match in database
@@ -61,6 +55,6 @@ public class MajorController {
 	@GetMapping("/{majorId}")
 	public ResponseEntity<ResponseForm> getMajorByMajorId(@PathVariable("majorId") long majorId , HttpServletRequest request){
 		ResponseForm result = majorService.getMajor(majorId , request);
-		return new ResponseEntity<>(result,result.getCode());
+		return new ResponseEntity<>(result,result.getHttpCode());
 	}
 }
