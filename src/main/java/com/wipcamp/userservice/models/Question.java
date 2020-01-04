e5d1045f95e7404c6a78ba68ebb0655058142400
Nope <o_o.toto@windowslive.com>
2020-01-04 18:09:48 +0700

    [UPDATE] Change JsonReference to Managed and Ignore List in Question

--- src/main/java/com/wipcamp/userservice/models/Question.java
+++ src/main/java/com/wipcamp/userservice/models/Question.java
@@ -1,6 +1,7 @@
 package com.wipcamp.userservice.models;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import org.springframework.lang.NonNull;
@@ -32,7 +33,7 @@ public class Question {
 	@JoinColumn(name="major_id",referencedColumnName = "id")
 	private Major major;
 
-	@JsonBackReference(value = "questionAnswers")
+	@JsonIgnore
 	@OneToMany(mappedBy = "question")
 	private List<Answer> answerList;
 
