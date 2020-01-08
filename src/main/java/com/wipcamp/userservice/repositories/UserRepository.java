c1f92a8574578ff092efd42f8ec2b29579df5c59
pipe34150 <pipe34150@hotmail.com>
2020-01-08 15:00:17 +0700

    [UPDATE] add find user by wip id in user repo

--- src/main/java/com/wipcamp/userservice/repositories/UserRepository.java
+++ src/main/java/com/wipcamp/userservice/repositories/UserRepository.java
@@ -9,4 +9,6 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User , Long> {
 
 	User findByLineId(Long lineId);
+
+	User findByWipId(long wipId);
 }
