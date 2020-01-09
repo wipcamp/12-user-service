ddd8d185cb3a3a61a2aa4ed4358727f8e58ee89e
pipe34150 <pipe34150@hotmail.com>
2020-01-10 01:34:01 +0700

    [UPDATE] add comment to test file

--- src/test/java/com/wipcamp/userservice/controllers/UserControllerTest.java
+++ src/test/java/com/wipcamp/userservice/controllers/UserControllerTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserControllerTest {
     @Test
     void getUserByToken() {
+    	//waiting for jwt decode for get user by token
     }
 
     @Test
