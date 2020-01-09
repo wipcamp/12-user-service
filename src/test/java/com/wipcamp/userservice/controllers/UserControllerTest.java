871f5ffb23d444a5be347c6e6ab783d9efb9a884
pipe34150 <pipe34150@hotmail.com>
2020-01-10 01:08:44 +0700

    [ADD] User Controller Test file

---
+++ src/test/java/com/wipcamp/userservice/controllers/UserControllerTest.java
@@ -0,0 +1,15 @@
+package com.wipcamp.userservice.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+    @Test
+    void getUserByToken() {
+    }
+
+    @Test
+    void updateUserByToken() {
+    }
+}
