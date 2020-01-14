770363134310b536a0a7d8c0f6015368a4f455a1
Nope <o_o.toto@windowslive.com>
2020-01-14 23:04:34 +0700

    [UPDATE] make findByLineId in UserRepository to return Optional Model

--- src/main/java/com/wipcamp/userservice/repositories/UserRepository.java
+++ src/main/java/com/wipcamp/userservice/repositories/UserRepository.java
@@ -5,10 +5,9 @@ import com.wipcamp.userservice.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User , Long> {
-
-	User findByLineId(Long lineId);
-
-	User findByWipId(long wipId);
+	public Optional<User> findByLineId(long lineId);
 }
