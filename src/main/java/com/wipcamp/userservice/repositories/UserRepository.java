b46a1c9cacb99b9c114a71bdd5718435cca66cd5
pipe34150 <pipe34150@hotmail.com>
2020-01-17 18:01:02 +0700

    [UPDATE] add findByCreatedAt in user repo

--- src/main/java/com/wipcamp/userservice/repositories/UserRepository.java
+++ src/main/java/com/wipcamp/userservice/repositories/UserRepository.java
@@ -5,9 +5,13 @@ import com.wipcamp.userservice.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User , Long> {
 	public Optional<User> findByLineId(long lineId);
+
+	public List<User> findByCreatedAt(Date thisDate);
 }
