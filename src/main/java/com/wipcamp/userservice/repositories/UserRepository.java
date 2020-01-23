cd52e773b1894a60bc9ab1f92140d07aa613db5b
pipe34150 <pipe34150@hotmail.com>
2020-01-23 16:21:48 +0700

    [UPDATE] add query by hour and daily

--- src/main/java/com/wipcamp/userservice/repositories/UserRepository.java
+++ src/main/java/com/wipcamp/userservice/repositories/UserRepository.java
@@ -3,9 +3,11 @@ package com.wipcamp.userservice.repositories;
 import com.wipcamp.userservice.models.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Date;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,5 +15,9 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User , Long> {
 	public Optional<User> findByLineId(long lineId);
 
-	public List<User> findByCreatedAt(Date thisDate);
+	@Query(value = "SELECT * FROM users WHERE DATE(created_at) = :date", nativeQuery=true)
+	public List<Integer> findDailyUser(@Param("date") String date);
+
+	@Query(value = "SELECT * FROM users WHERE DATE(created_at) = :date AND HOUR(created_at) = :hour", nativeQuery=true)
+	List<User> findUserPerHour(@Param("date") String date , @Param("hour") String hour);
 }
