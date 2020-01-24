955a41c7dff189da1b12b00c04556aafa7c7d91a
Nope <o_o.toto@windowslive.com>
2020-01-24 22:33:31 +0700

    [ADD] add PercentResponse class

---
+++ src/main/java/com/wipcamp/userservice/responses/PercentResponse.java
@@ -0,0 +1,16 @@
+package com.wipcamp.userservice.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class PercentResponse {
+	@Getter
+	@Setter
+	private int yesterday,today,percent;
+}
