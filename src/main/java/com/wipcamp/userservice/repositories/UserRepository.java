b58e6b07119ec712fd27aaf16d2afda209b71682
Nope <o_o.toto@windowslive.com>
2020-01-24 22:33:52 +0700

    [UPDATE] remove non used import from UserRepo

--- src/main/java/com/wipcamp/userservice/repositories/UserRepository.java
+++ src/main/java/com/wipcamp/userservice/repositories/UserRepository.java
@@ -2,14 +2,11 @@ package com.wipcamp.userservice.repositories;
 
 import com.wipcamp.userservice.models.User;
 
-import com.wipcamp.userservice.reponses.GraphDailyResponse;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
