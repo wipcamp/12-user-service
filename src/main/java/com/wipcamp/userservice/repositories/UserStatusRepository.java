29d9f9170f059772013e9e6b33ed77c7cbed3844
Nope <o_o.toto@windowslive.com>
2020-01-30 11:10:33 +0700

    [UPDATE] add countBy<allField> in UserStatusRepo

--- src/main/java/com/wipcamp/userservice/repositories/UserStatusRepository.java
+++ src/main/java/com/wipcamp/userservice/repositories/UserStatusRepository.java
@@ -5,4 +5,9 @@ import com.wipcamp.userservice.models.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserStatusRepository extends JpaRepository<UserStatus,Integer> {
+	Integer countByIsAccepted(boolean accepted);
+	Integer countByIsRegistered(boolean registered);
+	Integer countByIsGeneralAnswered(boolean generalAnswered);
+	Integer countByIsMajorAnswered(boolean majorAnswered);
+	Integer countByIsSubmitted(boolean submitted);
 }
