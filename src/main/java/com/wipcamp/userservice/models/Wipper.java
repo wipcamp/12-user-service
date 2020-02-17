c32af314943a589b012c123f86370719e4d3e481
pipe34150 <pipe34150@hotmail.com>
2020-02-17 13:56:40 +0700

    [ADD] wipper model

---
+++ src/main/java/com/wipcamp/userservice/models/Wipper.java
@@ -0,0 +1,33 @@
+package com.wipcamp.userservice.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "wippers")
+@Data
+@NoArgsConstructor
+public class Wipper {
+
+	@Id
+	@GeneratedValue
+	private Integer wipperId;
+
+	@Column(nullable = true)
+	private String name;
+
+	@Column(nullable = true)
+	private String surname;
+
+	@ManyToOne
+	@JoinColumn(name="major_id",referencedColumnName = "id")
+	private Role role;
+}
