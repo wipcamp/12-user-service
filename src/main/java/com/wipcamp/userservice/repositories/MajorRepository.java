40dd01f1a7a4d42b091da597d41ae950be7fff09
Nope <o_o.toto@windowslive.com>
2020-02-20 17:33:40 +0700

    [UPDATE] add findByName on MajorReps

--- src/main/java/com/wipcamp/userservice/repositories/MajorRepository.java
+++ src/main/java/com/wipcamp/userservice/repositories/MajorRepository.java
@@ -5,6 +5,11 @@ import com.wipcamp.userservice.models.Major;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.validation.constraints.NotNull;
+
+import java.util.Optional;
+
 @Repository
 public interface MajorRepository extends JpaRepository<Major, Long > {
+	Optional<Major> findByName(@NotNull String name);
 }
