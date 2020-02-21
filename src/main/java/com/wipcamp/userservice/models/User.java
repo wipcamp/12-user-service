4ea6625bc426be6e6f4c81455b139def12cd74c5
Nope <o_o.toto@windowslive.com>
2020-02-21 17:44:39 +0700

    [UPDATE] add length to User ComputerWorks to 1024

--- src/main/java/com/wipcamp/userservice/models/User.java
+++ src/main/java/com/wipcamp/userservice/models/User.java
@@ -20,6 +20,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
@@ -88,8 +89,11 @@ public class User {
 	private String congenitalDisease;
 	@Column(nullable = true)
 	private String congenitalDrug;
-	@Column(nullable = true)
+
+	@Lob
+	@Column(nullable = true, length=1024)
 	private String computerWorks;
+
 	@Column(nullable = true)
 	private String province;
 
